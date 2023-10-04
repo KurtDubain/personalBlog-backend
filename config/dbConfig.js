cd34050010b119a6b498a6484b46dcbd0d91cc71
KurtDubain <131502852+KurtDubain@users.noreply.github.com>
2023-10-04 22:46:09 +0800

    更新了数据库脚本
    为了避免数据库断开连接无法自动重连，解决了云服务器数据库不稳定的问题

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,27 +3,57 @@ const mysql = require('mysql2');
 // 数据库部分的配置
 
 // 数据库的连接的创建
-const connection = mysql.createConnection({
-  // host: 'localhost',
-  // user: 'blog',
-  // password: '20020220duyi+',
-  // database: 'blog',
-  host:'localhost',
-  user:'root',
-  password:'123456',
-  database:'blog'
-});
+// const connection = mysql.createConnection({
+//   // host: 'localhost',
+//   // user: 'blog',
+//   // password: '20020220duyi+',
+//   // database: 'blog',
+//   host:'localhost',
+//   user:'root',
+//   password:'123456',
+//   database:'blog'
+// });
 
 // const connection = pool.promise()
 
-connection.connect((err) => {
-  if (err) {
-    console.error('数据库连接失败');
-  } else {
-    console.log('数据库连接成功');
-  }
-});
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('数据库连接失败');
+//   } else {
+//     console.log('数据库连接成功');
+//   }
+// });
 
+function handleDisconnect() {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'blog'
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error('数据库连接失败');
+      console.error(err);
+      setTimeout(handleDisconnect, 2000); // 等待2秒后尝试重新连接
+    } else {
+      console.log('数据库连接成功');
+    }
+  });
+
+  connection.on('error', (err) => {
+    console.error('数据库连接错误');
+    console.error(err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect(); // 连接丢失时重新连接
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 
 module.exports = connection;
