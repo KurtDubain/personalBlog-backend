9b7cfca4010389a71de25d091cbc03755eff9368
KurtDubain <131502852+KurtDubain@users.noreply.github.com>
2023-12-05 23:59:37 +0800

    新增了system路由和对应的模型
    更新了system的四个方法，分别用于发送访问信息，获取访问信息

--- controllers/systemController.js
+++ controllers/systemController.js
@@ -2,48 +2,41 @@
 const systemModel = require('../models/systemModel');
 
 // 订阅信息的处理
-const SubItem = async (req, res) => {
-  
+const visitThat = async (req, res) => {
   try{
-    // 处理订阅成功的数据，返回成功信息
-      const subCondition = await subscriptionModel.SubItem(req.body);
-      res.status(200).json({ success: true, message: subCondition });
-
+    const { currentDate } = req.body
+    const userIP = req.ip
+    const userAgent = req.header['user-agent']
+    await systemModel.visitThat({ currentDate,userIP,userAgent })
+    res.status(200).json({success:true})
   }catch(error){
-    // 根据不同的错误信息进行返回
-    if (error.message === '用户未注册') {
-      res.status(400).json({ success: false, message: '用户未注册，请先注册' });
-    } else if (error.message === '该用户已经订阅') {
-      res.status(400).json({ success: false, message: '该用户已经订阅' });
-    } else if (error.message === '用户信息错误') {
-      res.status(400).json({ success: false, message: '用户信息错误，请检查姓名和账户' });
-    } else {
       // 其他异常，返回通用错误信息
       res.status(500).json({ success: false, message: '发生了服务器错误' });
-    }
+      console.error('访客记录记录失败',error);
   }
 };
 
 // 取消订阅的操作
-const UnSubItem = async (req, res) => {
-
+const getInfor = async (req, res) => {
   try{
-      const subCondition = await subscriptionModel.UnSubItem(req.body);
-      res.json({ message: '取消订阅成功',
-                subCondition });
+      const date = req.query.date
+      const todayNum = await getTodayVisitCount(date)
+      const totalNum = await getTotalVisitCount()
+      const weekData = await getWeekData()
+
+      res.status(200).json({
+        todayNum,
+        totalNum,
+        weekData
+      })
   }catch(error){
-    // 对不同的异常进行处理
-    if (error.message === '用户未订阅') {
-      res.status(400).json({ success: false, message: '用户未订阅，无法取消订阅' });
-    } else {
-      // 其他异常，返回通用错误信息
-      res.status(500).json({ success: false, message: '发生了服务器错误' });
-    }
+    res.status(500).json({success:false,message:'服务器错误'})
+    console.error('获取访客信息的时候失败',error)
   }
 };
 
 module.exports = {
-  SubItem,
-  UnSubItem
+  visitThat,
+  getInfor
 
 }
--- models/systemModel.js
+++ models/systemModel.js
@@ -1,89 +1,64 @@
 // models/subscriptionModel.js
 const db = require('../config/dbConfig')
 // 订阅模型
-// 判断用户是否已经注册，返回结果
-const isUserRegistered = async (form) => {
-  const selectSql = `SELECT * FROM users WHERE username = ? AND account = ?`;
-  const [rows, fields] = await db.promise().query(selectSql, [form.name, form.account]);
-  return rows.length > 0;
-};
 
-// 实现订阅操作
-const SubItem = (form) => {
+const visitThat = ({currentDate,userIP,userAgent}) => {
   return new Promise(async (resolve, reject) => {
-    try {
-      // 判断用户是否已注册
-      const isRegistered = await isUserRegistered(form);
-      if (!isRegistered) {
-        reject(new Error('用户未注册'));
-        return;
-      }
-      // 若用户已注册，则执行信息插入操作
-      const selectSql = `SELECT * FROM subscriptions WHERE name = ? OR account = ?`;
-      db.promise()
-        .query(selectSql, [form.name, form.account])
-        .then(([rows, fields]) => {
-          // rows 是查询结果的数据部分
-          if (rows.length > 0) {
-            // 判断是否存在该用户
-            const existingSubscription = rows.find(
-              (row) => row.name === form.name && row.account === form.account
-            );
-            if (existingSubscription) {
-              reject(new Error('该用户已经订阅'));
-            } else {
-              reject(new Error('用户信息错误'));
-            }
-          } else {
-            // 用户不存在，插入订阅信息
-            const insertSql = `INSERT INTO subscriptions (name, account) VALUES (?, ?)`;
-            return db.promise().query(insertSql, [form.name, form.account]);
-          }
-        })
-        .then(() => {
-          resolve('该用户订阅成功');
-        })
-        .catch((error) => {
-          console.error(error);
-          reject(error);
-        });
-    } catch (error) {
-      console.error(error);
-    }
+    const insertSql = 'insert into visitor_data (visit_time,ip,user_agent) values(?,?,?)'
+    db.promise()
+      .query(insertSql,[currentDate,userIP,userAgent])
+      .then(resolve())
+      .catch(error=>{
+        console.error(error,'访问信息数据库插入异常');
+        reject(error)
+      })
   });
 };
 
 // 取消订阅操作
-const UnSubItem = (form) => {
+const getTodayVisitCount = (date) => {
   return new Promise((resolve, reject) => {
-    try {
-      const selectSql = `SELECT * FROM subscriptions WHERE name = ? AND account = ?`;
-      db.promise()
-        .query(selectSql, [form.name, form.account])
-        .then(([rows, fields]) => {
-          if (rows.length === 0) {
-            // 直接判断是否存在，未存在则抛出异常
-            reject(new Error('用户未订阅'));
-          } else {
-            // 执行delete命令直接删除用户数据
-            const deleteSql = `DELETE FROM subscriptions WHERE name = ? AND account = ?`;
-            return db.promise().query(deleteSql, [form.name, form.account]);
-          }
-        })
-        .then(() => {
-          resolve('用户删除成功');
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    } catch (error) {
-      console.error(error);
-    }
+    const selectSql = 'select count(*) as count from visitor_data where date(visit_time) = ?'
+    db.promise()
+      .query(selectSql,[date])
+      .then(([rows])=>resolve(rows[0].count))
+      .catch(error=>{
+        console.error('数据库查找当日访问信息失败',error);
+        reject(error)
+      })
   });
 };
 
+const getTotalVisitCount = ()=>{
+  return new Promise((resolve,reject)=>{
+    const selectSql = 'select count(*) as count from visitor_data'
+    db.promise()
+      .query(selectSql)
+      .then(([rows])=>resolve(rows[0].count))
+      .catch(error=>{
+        console.error('数据库查找总访问信息失败',error);
+        reject(error)
+      })
+  })
+}
+
+const getWeekData = () =>{
+  return new Promise((resolve,reject)=>{
+    const selectSql = 'SELECT DATE(visit_time) as day, COUNT(*) as count FROM visitor_data WHERE visit_time >= CURDATE() - INTERVAL 6 DAY GROUP BY day'
+    db.promise()
+      .query(selectSql)
+      .then(([rows])=>resolve(rows))
+      .catch(error=>{
+        console.error('数据库查找一周访问信息失败',error);
+        reject(error)
+      })
+  })
+}
+
 
 module.exports = {
-  SubItem,
-  UnSubItem
+  visitThat,
+  getTodayVisitCount,
+  getTotalVisitCount,
+  getWeekData
 };
